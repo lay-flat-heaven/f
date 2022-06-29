972aab19865b5e7d4d0ebe432b14ea763963626f
zhouchengxin <1985859942@qq.com>
2022-06-29 16:05:46 +0800

    intro card

--- src/components/Start.vue
+++ src/components/Start.vue
@@ -11,19 +11,69 @@
                     <img src="../assets/picture1.png" class="picture1">
                     &nbsp&nbsp&nbspstep1
                 </p>
-                <p class="stepcontent">select pictures of model and cloth in your device which meets the requirements to apply them</p>
+                <p class="stepcontent">
+                    select pictures of model and cloth in your device which meets the requirements to apply them
+                </p>
             </div>
             <div class="part2">
-                HelloWorldnsf📄📄
+                <div class="part2content">
+                    <div class="part2picture">
+                        <img src="../assets/picture3.png" class="picture2">
+                    </div>
+                    <div class="part2text">
+                        <p class="part2texttitle">Efficiency</p>
+                        <p class="part2textcontent">
+                            just a single second is absolutely adequate to a gengeration
+                        </p>
+                    </div>
+                </div>
+                <div class="part2content">
+                    <div class="part2picture">
+                        <img src="../assets/picture5.png" class="picture2">
+                    </div>
+                    <div class="part2text">
+                        <p class="part2texttitle">Mobility</p>
+                        <p class="part2textcontent">
+                            with the offered pre-trained parameters , even a cellphone is sufficient to run Futurama
+                        </p>
+                    </div>
+                </div>
             </div>
         </el-col>
         <el-col :span="12">
             <div class="part1">
-                <p class="step">step2</p>
-                φ(*￣0￣)( •̀ ω •́ )✧(～￣▽￣)(～￣▽￣)～（￣︶￣）↗　(￣︶￣)↗[GO!][]~(￣▽￣)~*
+                <p class="step">
+                    <img src="../assets/picture2.png" class="picture1">
+                    &nbsp&nbsp&nbspstep2
+                </p>
+                <p class="stepcontent">
+                    fill the images into the fitting block and just click the "generate" button
+                </p>
+                
             </div>
             <div class="part2">
-                HelloWorlf📄📄
+                <div class="part2content">
+                    <div class="part2picture">
+                        <img src="../assets/picture4.png" class="picture2" style="top:25px;">
+                    </div>
+                    <div class="part2text">
+                        <p class="part2texttitle">Foresight</p>
+                        <p class="part2textcontent">
+                            pictures in wild are on their way to availability
+                        </p>
+                    </div>
+                </div>
+                <div class="part2content">
+                    <div class="part2picture">
+                        <img src="../assets/picture6.png" class="picture2">
+                    </div>
+                    <div class="part2text">
+                        <p class="part2texttitle">Accessibility</p>
+                        <p class="part2textcontent">
+                            get started simply with images of the cloth and the model.
+                        </p>
+                    </div>      
+                </div>
             </div>
         </el-col>
     </el-row>
@@ -45,11 +95,43 @@ export default {
 </script>
 
 <style>
+.part2picture{
+    position: relative;
+    text-align: center;
+    left: 30px;
+    float: left;
+    width: 33%;
+    height: 100%;
+}
+.part2text{
+    position: relative;
+    top: 30px;
+    left:30px;
+    float: left;
+    width: 50%;
+    height: 100%;
+}
+.part2texttitle{
+    position: relative;
+    text-align: left;
+    font-size: 25px;
+    height: 5px;
+    color:black;
+    font-weight:bolder;
+}
+.part2content{
+    width: 67%;
+}
 .picture1{
     width:25px;
     height:25px;
     position:relative;
     top: 5px;
+}
+.picture2{
+    position: relative;
+    top:45px;
+    width: 70%;
 }
 .part1{
     background-color: rgb(255, 255, 255);
@@ -57,7 +139,7 @@ export default {
     margin: 0 auto;
     border-radius:5px;
     width: 80%;
-    height: 30%;
+    height: 22%;
 }
 .step{
     position: relative;
@@ -74,12 +156,24 @@ export default {
     font-weight: 100;
     text-align: left;
     font-size: 20px;
-    top: 25px;
+    top: 40px;
+    left: 30px;
+    width: 90%;
 }
 .part2{
     position: relative;
     color: black;
-    top:50px;
+    text-align: left;
+    height: 220px;
+    top:10px;
+}
+.part2content{
+    position: relative;
+    margin: 0 auto;
+    justify-content:center;
+    width: 80%;
+    top:25px;
+    height:170px;
 }
 .illustrate {
     position: relative;
